37fa4a69f0785d1ebef67b2b283b6107662d23b5
Ophélie Gaudin <ophelie.gaudin@outlook.fr>
2021-11-30 14:51:06 +0100

    Create cart show view

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,7 +1,21 @@
 class CartController < ApplicationController
 
   def show
-    # @user = 
+    @user = current_user;
+    @cart = Cart.find(params[:id]);
+    puts "############################################################################"
+    puts current_user
+    puts @cart
+    puts "############################################################################"
+    @items = @cart.items
+    
+    total = 0
+    @items.each do |item|
+    total += item.price
+    end
+
+    @cart_total = total
+
   end
 
   
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   resources :home
   root to: 'home#index'
   devise_for :users
-  resources :carts
+  resources :cart
+  # resources :join_table_item_carts
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
