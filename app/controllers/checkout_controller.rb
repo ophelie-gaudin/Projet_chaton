adf8d0ba65a2b9e8583b198305843b849813c74c
Istarengwa <muller.lilian@outlook.fr>
2021-12-01 23:12:36 +0100

    add test for mailing order

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,4 +1,6 @@
 class CheckoutController < ApplicationController
+  before_action :authenticate_user!
+
   def new
     @card = Cart.find_by(user_id: current_user.id)
     @stripe_amount = 500
@@ -10,56 +12,53 @@ class CheckoutController < ApplicationController
     @items.each do |item|
       total += item.price
       end
-  @cart_total = total
+    @cart_total = total
     
-  @amount = @cart_total
-  puts @amount
+    @amount = @cart_total
+    puts @amount
     @stripe_amount = (@amount * 100).to_i
-   puts @stripe_amount
+    puts @stripe_amount
   end
 
   def create
   # Before the rescue, at the beginning of the method
-  @card = Cart.find_by(user_id: current_user.id)
-  @stripe_amount = 500
-  @user = current_user
-  @items = @card.items
-  puts @items
-  puts '----------------------------------------'
-  total = 0
-  @items.each do |item|
-    total += item.price
+    @card = Cart.find_by(user_id: current_user.id)
+    @stripe_amount = 500
+    @user = current_user
+    @items = @card.items
+    puts @items
+    puts '----------------------------------------'
+    total = 0
+    @items.each do |item|
+      total += item.price
     end
-@cart_total = total
+    @cart_total = total
   
-@amount = @cart_total
-puts @amount
-  @stripe_amount = (@amount * 100).to_i
- puts @stripe_amount
-  begin 
+    @amount = @cart_total
+    puts @amount
+    @stripe_amount = (@amount * 100).to_i
+    puts @stripe_amount
+    
+    begin 
   
-  customer = Stripe::Customer.create({
-  email: params[:stripeEmail],
-  source: params[:stripeToken],
-  })  
-  charge = Stripe::Charge.create({
-  customer: customer.id,
-  amount: @stripe_amount,
-  description: "Achat d'un produit",
-  currency: 'eur',
-  })
+      customer = Stripe::Customer.create({
+      email: params[:stripeEmail],
+      source: params[:stripeToken],
+      })  
+      charge = Stripe::Charge.create({
+      customer: customer.id,
+      amount: @stripe_amount,
+      description: "Achat d'un produit",
+      currency: 'eur',
+      })
   
-  rescue Stripe::CardError => e
-  flash[:error] = e.message
-  redirect_to new_order_path
-  end
-  
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to new_checkout_path
+    end
+    
 # After the rescue, if the payment succeeded
-  @relations= JoinTableItemCart.where(cart_id: Cart.find_by(user_id: current_user.id).id).destroy_all
+    @relations= JoinTableItemCart.where(cart_id: Cart.find_by(user_id: current_user.id).id).destroy_all
   
-  end
-
-  def success
-    @order = Order.create(user: current_user)
   end
 end
