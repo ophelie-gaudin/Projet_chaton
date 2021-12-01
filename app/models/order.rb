08f24c075398503e029b09ff651a473ba4d447f6
Istarengwa <muller.lilian@outlook.fr>
2021-12-01 14:04:42 +0100

    create mail user & admin for order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,14 @@
 class Order < ApplicationRecord
   belongs_to :user
+
+  after_create :order_user_confirmation_email
+  after_create :order_admin_confirmation_email
+
+  def order_user_confirmation_email
+    UserMailer.order_confirmation_email(self.user_id, self).deliver_now
+  end
+  
+  def order_admin_confirmation_email
+    UserMailer.copy_order_admin(self.user_id, self).deliver_now
+  end
 end
