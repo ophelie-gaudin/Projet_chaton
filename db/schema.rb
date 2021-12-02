42919932e2c8e3b1e9e54cf169002ea8c38d5f0d
Ophélie Gaudin <ophelie.gaudin@outlook.fr>
2021-12-02 11:37:13 +0100

    fix conflicts (merge)

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2021_12_02_091832) do
-=======
-ActiveRecord::Schema.define(version: 2021_12_01_151241) do
->>>>>>> development
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
