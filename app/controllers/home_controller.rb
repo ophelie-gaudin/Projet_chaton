494f219980e8fbd4f6d0ac5465a9b0ad49d6fa39
Tariok <mingotevan@gmail.com>
2021-12-02 21:06:36 +0100

    6eme

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,5 @@
 class HomeController < ApplicationController 
   def index
     @items = Item.all  
-    @cart = Cart.find_by(user_id: current_user.id)
-    @itemscart = @cart.item
-    @iditemcart = @itemscart.map{|x| x.id}
-    
   end
 end
