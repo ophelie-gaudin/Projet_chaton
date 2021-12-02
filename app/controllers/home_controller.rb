a2020e9e88c2429526054d4992059b03a5142a24
MidNightRoad <bergerpierre09@gmail.com>
2021-12-02 18:26:52 +0100

    add feature delete cart if in cart

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController 
   def index
     @items = Item.all  
+    @cart = Cart.find_by(user_id: current_user.id)
+    @itemscart = @cart.items
+    @iditemcart = @itemscart.map{|x| x.id}
     
   end
 end
